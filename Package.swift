fe3ad6ea12c8a0c5361d852bd963f8628274e149
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 11:08:04 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,15 +1,15 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
 let package = Package(
     name: "swift-parser-effect-primitives",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27"),
-        .visionOS("27")
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
+        .visionOS(.v27),
     ],
     products: [
         .library(
@@ -22,8 +22,14 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-primitives/swift-parser-primitives.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-effect-primitives.git", branch: "main"),
+        .package(
+            url: "https://github.com/swift-primitives/swift-parser-primitives.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-effect-primitives.git",
+            branch: "main"
+        ),
     ],
     targets: [
         .target(
@@ -36,7 +42,7 @@ let package = Package(
         .target(
             name: "Parser Backtrack Primitives Test Support",
             dependencies: [
-                "Parser Backtrack Primitives",
+                "Parser Backtrack Primitives"
             ],
             path: "Tests/Support"
         ),
@@ -45,7 +51,10 @@ let package = Package(
             dependencies: [
                 "Parser Backtrack Primitives",
                 "Parser Backtrack Primitives Test Support",
-                .product(name: "Parser Primitives Test Support", package: "swift-parser-primitives"),
+                .product(
+                    name: "Parser Primitives Test Support",
+                    package: "swift-parser-primitives"
+                ),
             ]
         ),
     ],
